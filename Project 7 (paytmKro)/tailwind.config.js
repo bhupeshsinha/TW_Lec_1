33b3cecc8692b8fcaef1778881e04f62617112a9
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-05-30 17:10:05 +0530

    landing page complete

---
+++ Project 7 (paytmKro)/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        dark_nilla: "#002970",
+        nilla:"#00baf2",
+        lightColor:"#f5f5f5"
+      }
+    },
+  },
+  plugins: [],
+}
+
+
