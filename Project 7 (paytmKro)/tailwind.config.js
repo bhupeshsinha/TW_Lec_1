21a058b2e1d3de33e74061ac16d90f8fabbefb3c
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-06-01 08:00:45 +0530

    3 card views done

--- Project 7 (paytmKro)/tailwind.config.js
+++ Project 7 (paytmKro)/tailwind.config.js
@@ -6,8 +6,10 @@ module.exports = {
       colors:{
         dark_nilla: "#002970",
         nilla:"#00baf2",
-        nillBlur:"#00a5d6",
-        lightColor:"#f5f5f5"
+        nillBlur:"#00afe3",
+        lightColor:"#f5f5f5",
+        lightDarkNilla:"#0f4a8a",
+        bgColor:"#f5f7fa"
       }
     },
   },
